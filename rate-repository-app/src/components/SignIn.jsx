490b4b0e3538b530ad80a9ec8295123c8d70e042
tc849948 <student@developer.com>
2026-09-17 13:48:06 +0300

    Exercise 13: The sign in form mutation

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -4,6 +4,7 @@ import * as yup from 'yup';
 
 import Text from './Text';
 import theme from '../theme';
+import useSignIn from '../hooks/useSignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -47,8 +48,17 @@ const validationSchema = yup.object().shape({
 });
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log(values);
+  const [signIn] = useSignIn();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const formik = useFormik({
